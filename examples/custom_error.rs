6cf88ea788b976642b74ab20b964401b6c05696a
Michael Mc Donnell <michael@mcdonnell.dk>
2022-02-25 09:36:28 -0800

    Expand custom error example
    I felt like the custom error example didn't show enough about how to use
    the custom error type. I added more code to show how to emit the `MyError`
    value.

--- examples/custom_error.rs
+++ examples/custom_error.rs
@@ -1,8 +1,11 @@
 extern crate nom;
 
+use nom::bytes::complete::tag;
 use nom::error::ErrorKind;
 use nom::error::ParseError;
+use nom::sequence::tuple;
 use nom::Err::Error;
+use nom::Finish;
 use nom::IResult;
 
 #[derive(Debug, PartialEq)]
@@ -21,8 +24,44 @@ impl<I> ParseError<I> for CustomError<I> {
   }
 }
 
-fn parse(input: &str) -> IResult<&str, &str, CustomError<&str>> {
-  Err(Error(CustomError::MyError))
+fn foo(input: &str) -> IResult<&str, &str, CustomError<&str>> {
+  let (i, o) = tag("foo")(input)?;
+  Ok((i, o))
+}
+
+fn bar(input: &str) -> IResult<&str, &str, CustomError<&str>> {
+  /*
+  let (i, o) = tag::<_, _, CustomError<_>>("bar")(input)
+    .map_err(|_e| nom::Err::Error(CustomError::MyError))?;
+  Ok((i, o))
+  */
+  match tag::<_, _, CustomError<_>>("bar")(input) {
+    Ok((i, o)) => Ok((i, o)),
+    _ => Err(Error(CustomError::MyError)),
+  }
+}
+
+#[derive(Debug)]
+struct FooBar<'a>(&'a str, &'a str);
+
+fn parse(input: &str) -> IResult<&str, FooBar, CustomError<&str>> {
+  let (i, (foo, bar)) = tuple((foo, bar))(input)?;
+
+  Ok((i, FooBar(foo, bar)))
+}
+
+fn main() {
+  let res = parse("foo42bar").finish();
+  match res {
+    Ok((i, foo_bar)) => println!(
+      "Input is '{}' and output is ({}, {})",
+      i, foo_bar.0, foo_bar.1
+    ),
+    Err(CustomError::MyError) => println!("Got MyError"),
+    Err(CustomError::Nom(s, error_kind)) => {
+      println!("Got Nom error: '{}', '{:?}'", s, error_kind)
+    }
+  };
 }
 
 #[cfg(test)]
@@ -33,7 +72,7 @@ mod tests {
 
   #[test]
   fn it_works() {
-    let err = parse("").unwrap_err();
+    let err = parse("foo42bar").unwrap_err();
     match err {
       Error(e) => assert_eq!(e, CustomError::MyError),
       _ => panic!("Unexpected error: {:?}", err),
